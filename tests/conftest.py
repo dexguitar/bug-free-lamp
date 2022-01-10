1a1b64ff7364b41db09a9046cd6e785d5711f766
dexguitar <stinzane@gmail.com>
2022-01-10 10:55:04 +0300

    initial commit

---
+++ tests/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+@pytest.fixture(scope="function")
+def browser():
+    options = Options()
+    options.add_argument("--lang=en")
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome(chrome_options=options)
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
